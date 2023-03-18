fb06702b403584eafff499cb93bd2c9bef3d95a6
David Zhi LuoZhang <51495738+davidluozhang@users.noreply.github.com>
2023-03-18 14:14:12 -0400

    larger kernel model

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,32 @@
 import torch
+from torch import nn
 
 
 class Model(torch.nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        super(Model, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=num_channels,
+                               out_channels=32, kernel_size=5, padding=1)
+        self.pool1 = nn.MaxPool2d(2, stride=2)
+        self.ReLU = nn.ReLU()
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=4, padding=2)
+        self.pool2 = nn.MaxPool2d(3, stride=2)
+        self.flatten = nn.Flatten(1, 3)
+        self.linear1 = nn.Linear(3136, 256, True)
+        self.linear2 = nn.Linear(256, 128, True)
+        self.linear3 = nn.Linear(128, num_classes, True)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.ReLU(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = self.flatten(x)
+        x = self.linear1(x)
+        x = self.ReLU(x)
+        x = self.linear2(x)
+        x = self.ReLU(x)
+        x = self.linear3(x)
+        return x
--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -2,7 +2,7 @@ import torch
 from typing import Callable
 
 
-class MLP:
+class MLP(torch.nn.Module):
     def __init__(
         self,
         input_size: int,
@@ -22,7 +22,31 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super().__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_classes = num_classes
+        self.hidden_count = hidden_count
+        self.activation = activation
+        self.initializer = initializer
+
+        hidden_layers = []
+        for i in range(self.hidden_count):
+            if i == 0:
+                hidden_layers.append(
+                    torch.nn.Linear(self.input_size, self.hidden_size)
+                )
+            else:
+                hidden_layers.append(
+                    torch.nn.Linear(self.hidden_size, self.hidden_size)
+                )
+            hidden_layers.append(self.activation())
+        
+        self.hidden_layers = torch.nn.Sequential(*hidden_layers)
+        self.output_layer = torch.nn.Linear(self.hidden_size, self.num_classes)
+        initializer(self.output_layer.weight)
+
 
     def forward(self, x):
         """
@@ -34,4 +58,4 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        return self.output_layer(self.hidden_layers(x))
