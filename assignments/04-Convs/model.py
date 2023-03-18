ae86f4af39f20d187b3c13ab30687b95cfac8c75
David Zhi LuoZhang <51495738+davidluozhang@users.noreply.github.com>
2023-03-18 14:26:35 -0400

    parameter reduction in the linear layers, reduced # channels in conv

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -6,16 +6,16 @@ class Model(torch.nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=num_channels,
-                               out_channels=32, kernel_size=5, padding=1)
+                               out_channels=16, kernel_size=3, padding=1)
         self.pool1 = nn.MaxPool2d(2, stride=2)
         self.ReLU = nn.ReLU()
         self.conv2 = nn.Conv2d(
-            in_channels=32, out_channels=64, kernel_size=4, padding=2)
-        self.pool2 = nn.MaxPool2d(3, stride=2)
+            in_channels=16, out_channels=32, kernel_size=3, padding=2)
+        self.pool2 = nn.MaxPool2d(2, stride=2)
         self.flatten = nn.Flatten(1, 3)
-        self.linear1 = nn.Linear(3136, 256, True)
-        self.linear2 = nn.Linear(256, 128, True)
-        self.linear3 = nn.Linear(128, num_classes, True)
+        self.linear1 = nn.Linear(2592, 128, True)
+        self.linear2 = nn.Linear(128, 64, True)
+        self.linear3 = nn.Linear(64, num_classes, True)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv1(x)
